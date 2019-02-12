96502eb7c7ed1ec6a08b14b5342fa86436400a82
Dark-Raine <anderson-tomas@hotmail.com>
2019-02-12 15:43:40 +0000

    front end login page setup, no functionality

---
+++ FrontEnd/assets/scripts/index.js
@@ -0,0 +1,5 @@
+const loginPageEl = document.body.querySelector('#login-page')
+const usrNameEl = document.body.querySelector('#usr-input')
+const loginBtnEl = document.body.querySelector('#login-btn')
+
+
